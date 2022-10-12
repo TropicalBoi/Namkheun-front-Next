f226234f6d348a37a02922e1b7a98232217cf8a2
TropicalBoi <tewprai.bualoi@gmail.com>
2022-10-12 19:04:32 +0700

    cant call coverImg yet

--- pages/projects.js
+++ pages/projects.js
@@ -19,13 +19,35 @@ const Projects = () => {
     const manifestoData = await axios.get(
       "https://namkheun-back.herokuapp.com/api/manifestos?populate=%2A"
     );
-    SetManifestos(manifestoData.data);
+    SetManifestos(manifestoData.data.data);
   };
 
   useEffect(() => {
     fetch();
     fetchManifesto();
   }, []);
+
+  const renderProjectBody = (projectsId) => {
+    switch (projectsId) {
+      case "1":
+        return (
+          <div className={style.projectBody}>
+            {manifestos.map((Manifesto) => {
+              return (
+                <picture>
+                  <img
+                    src={Manifesto.attributes.Images.data.attributes.url}
+                    className={style.manifestosCoverImg}
+                  />
+                </picture>
+              );
+            })}
+          </div>
+        );
+      default:
+        return <></>;
+    }
+  };
 
   return (
     <Layout>
@@ -45,6 +67,7 @@ const Projects = () => {
                     {Project.attributes.ProjectName}
                   </p>
                 </div>
+                {renderProjectBody(projects.id)}
               </div>
             );
           })}
