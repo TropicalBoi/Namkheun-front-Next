66e8f947b857e2638790bb33ce218ae331e6b96a
TropicalBoi <tewprai.bualoi@gmail.com>
2022-11-30 18:18:46 +0700

    subscribe

--- pages/contact.js
+++ pages/contact.js
@@ -1,70 +1,61 @@
 import React from "react";
 import Layout from "../src/components/layout";
-import style from '../styles/contact.module.css'
+import style from "../styles/contact.module.css";
 
 const Contact = () => {
   return (
     <Layout>
-    <div
-      className={style.contactContainer}
-    >
-      <p
-        className={style.signupSection}
-      >
-        Sign up for<br></br>
-        our mailing list:<br></br>
-        <input
-          className={style.inputSection}
-          type="text"
-          placeholder="Your email here"
-        />
-        <br></br>
-        <input
-          className={style.summitSection}
-          type="submit"
-          value="Subscibe"
-        />
-      </p>
-      <p
-        className={style.socialUpdate}
-      >
-        For regular updates, follow us on
-        <br></br>
-        <br></br>
-        IG{" "}
-        <a
-          href="https://www.instagram.com/namkheun/"
-          target="_blank"
-          
-          rel="noreferrer"
-        >
-          @namkheun
-        </a>
-        <br></br>
-        Twitter{" "}
-        <a
-          href="https://twitter.com/namkheun"
-          target="_blank"
-          
-          rel="noreferrer"
-        >
-          @namkheun
-        </a>
-        <br></br>
-        FB{" "}
-        <a
-          href="https://www.facebook.com/namkheun"
-          target="_blank"
-          
-          rel="noreferrer"
-        >
-          <span>น้ำขึ้น</span> Namkheun
-        </a>
-        <br></br>
-        <br></br>
-        Alternatively, write to us at collective@namkheun.com
-      </p>
-    </div>
+      <div className={style.contactContainer}>
+        <p className={style.signupSection}>
+          Sign up for<br></br>
+          our mailing list:<br></br>
+          <input
+            className={style.inputSection}
+            type="text"
+            placeholder="Your email here"
+          />
+          <br></br>
+          <input
+            className={style.summitSection}
+            type="submit"
+            value="Subscribe"
+          />
+        </p>
+        <p className={style.socialUpdate}>
+          For regular updates, follow us on
+          <br></br>
+          <br></br>
+          IG{" "}
+          <a
+            href="https://www.instagram.com/namkheun/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            @namkheun
+          </a>
+          <br></br>
+          Twitter{" "}
+          <a
+            href="https://twitter.com/namkheun"
+            target="_blank"
+            rel="noreferrer"
+          >
+            @namkheun
+          </a>
+          <br></br>
+          FB{" "}
+          <a
+            href="https://www.facebook.com/namkheun"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span>น้ำขึ้น</span> Namkheun
+          </a>
+          <br></br>
+          <br></br>
+          Alternatively, write to us at collective@namkheun.com
+        </p>
+      </div>
     </Layout>
   );
 };
