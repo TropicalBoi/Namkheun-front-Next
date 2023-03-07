fa5744a574dfa7979da4e98942bc2984d8b48de6
TropicalBoi <tewprai.bualoi@gmail.com>
2023-03-07 09:38:14 +0700

    about text quick change

--- pages/about.js
+++ pages/about.js
@@ -33,7 +33,7 @@ const About = () => {
             </div>
             {engText && (
               <p className="engTxt">
-                Namkheun is translated as ‘rising tides’,
+                Translated as ‘Rising Tides’,
                 <span> (น้ำขึ้น) </span>
                 is a literary collective based in Bangkok, Thailand.
                 <br></br>
@@ -42,11 +42,8 @@ const About = () => {
                 over-the-phone gossip sessions between two friends over the
                 years. Together, we raged over the state of the world. Together,
                 we shared memes, unfulfilled dreams, and personal insights. And
-                so in 2020,
-                <br></br>
-                <br></br>
-                Namkheun materialised as an extension of such activities, an
-                open chat if you will.
+                so in 2020, Namkheun materialised as an extension of such
+                activities, an open chat if you will.
                 <br></br>
                 <br></br>
               </p>
@@ -55,16 +52,14 @@ const About = () => {
             {!engText && (
               <p className="thaiTxt">
                 น้ำขึ้นเป็นคอลเลคทีฟที่ทำงานกับตัวหนังสือ
-                น้ำขึ้นก่อตัวจากการเม้ามอยโฟ่ฝอยระหว่าง
-                เพื่อนสนิทสองคนตลอดระยะเวลาหลายปีที่ผ่านมา
-                (และก็ยังคงเป็นอย่างนั้นแหละ) เราบ่น เราฉอด
-                เราฟาดสารพัดสิ่งเพราะทุกเรื่องที่ (ไม่) มีสาระเริ่มต้นด้วย
-                “มึงง” และจบด้วย “อีดอก” เราส่งมีม ส่งต่อความคลั่งรักส์
-                ความในจัยและความฝันเพ้อพก
-                ถึงน้ำขึ้นคอลเลคทีฟจะมีตัวตนขึ้นมาเมื่อปี 2020 ที่ผ่านมา
-                เท่านั้นเอง
+                น้ำขึ้นก่อตัวจากการเม้ามอยโฟ่ฝอยระหว่างเพื่อนสนิทสองคนตลอดระยะเวลาหลายปีที่ผ่านมา
+                (และก็ยังคงเป็นอย่างนั้นแหละ) เราบ่น เราฉอด เราฟาดสารพัดสิ่ง
+                เพราะทุกเรื่องที่(ไม่)มีสาระเริ่มต้นด้วย “มึงง” และจบด้วย
+                “อีดอก” เราส่งมีม ส่งต่อความคลั่งรักส์ ความในจัยและความฝันเพ้อพก
+                ถึงน้ำขึ้นคอลเลคทีฟจะมีตัวตนขึ้นมาเมื่อปี 2020
+                ที่ผ่านมาเท่านั้นเอง
                 เราก็มองว่าน้ำขึ้นคือส่วนขยายของสารพัดสิ่งที่เราทำด้วยกันอยู่แล้ว
-                เป็นเหมือนโอเพ่นแชทก็ได้
+                จะเป็นโอเพ่นแชทก็ได้
               </p>
             )}
           </div>
